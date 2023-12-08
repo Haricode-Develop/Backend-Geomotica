ee69a2049233ee126f01d9c07b4ff542163957a5
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2023-12-08 00:11:47 -0600

    Update dashboardController.js

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,6 +1,7 @@
 const DashboardModel = require('../models/dashboard');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const {exec} = require('child_process');
 
