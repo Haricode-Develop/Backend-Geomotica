758aa58abc9a6776465dd463da6d89aa10ddd935
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2023-12-27 21:24:09 -0600

    add

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -39,19 +39,21 @@ const execBash = async (req, res) => {
                 if (error) {
                     console.error(`exec error: ${error}`);
                     reject(`Error executing script: ${error.message}`);
-                    return;
+                } else {
+                    if (stderr) {
+                        console.error(`stderr: ${stderr}`);
+                    }
+                    if (esPrimeraEjecucion) {
+                        console.log("SE HA EJECUTADO EL EVENTO PARA MOSTRAR EL ANÁLISIS ======");
+                        io.getIo().emit('datosInsertados');
+                        esPrimeraEjecucion = false;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                    resolve();
                 }
-                if (stderr) {
-                    console.error(`stderr: ${stderr}`);
-                }
-                console.log(`stdout: ${stdout}`);
-                if (esPrimeraEjecucion) {
-                    console.log("SE HA EJECUTADO EL EVENTO PARA MOSTRAR EL ANÁLISIS ======");
-                    io.getIo().emit('datosInsertados');
-                    esPrimeraEjecucion = false;
-                }
-                resolve();
+                
             });
+
         });
         res.send('Script executed successfully');
     } catch (error) {
