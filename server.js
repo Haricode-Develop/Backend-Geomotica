d6f3d4c3118613d282ab08580b6b3dd8161f3ce1
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2024-01-04 22:41:36 -0600

    Contenido json
    Add, adjunto actualización para aceptar json de mayor tamaño.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const http = require('http');
 const morgan = require('morgan');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 3001;
 require('dotenv').config();
 
@@ -12,7 +13,9 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 const socketRoutes = require('./routes/webSocket');
 
 app.use(express.urlencoded({ extended: true })); // Para parsear application/x-www-form-urlencoded
-app.use(morgan('combined')); 
+app.use(morgan('combined'));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.json());
 
