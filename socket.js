86e8313cfffef92843974041266486580cfc8625
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2024-02-09 18:21:22 -0600

    cors socket
    Add, Añado los cors para el socket.

--- socket.js
+++ socket.js
@@ -1,8 +1,18 @@
+// socket.js
+const socketIO = require('socket.io');
+
 let io;
 
 module.exports = {
     init: (server) => {
-        io = require('socket.io')(server);
+        io = socketIO(server, {
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"],
+                allowedHeaders: ["my-custom-header"],
+                credentials: true
+            }
+        });
 
         io.on('connection', (socket) => {
             console.log('Un cliente se ha conectado');
