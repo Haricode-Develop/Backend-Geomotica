43a4278fa05699a83b46bd3db400d9609b823bd6
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2024-03-17 17:06:14 -0600

    Historial

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -24,48 +24,52 @@ const analisis = async(req, res) => {
 const obtenerArchivoTIFF = async (req, res) => {
     const nombreAnalisis = req.params.nombreAnalisis;
     const id = req.params.id;
-    const archivoNombre = `raster/${nombreAnalisis}_${id}.tif`;
-    const nombreTabla = "cosecha_mecanica";
+    const archivoNombre = `${nombreAnalisis}_${id}.tif`;
+    const archivo = bucket.file(archivoNombre);
+
+    console.log("ESTOS SON LOS PARAMETROS: ");
+    console.log("NOMBRE DEL ARCHIVO: ", archivoNombre);
+
     try {
-        console.log("Buscando archivo: ", archivoNombre);
-        const archivo = bucket.file(archivoNombre);
-        const existeArchivo = await archivo.exists();
-
-        if (!existeArchivo[0]) {
+        const [existeArchivo] = await archivo.exists();
+        if (!existeArchivo) {
             console.log("El archivo no existe, generando...");
-            const comandoPython = `python3 /geomotica/procesos/generar_raster.py ${id} ${nombreTabla}`;
 
-            exec(comandoPython, async (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`Error: ${error.message}`);
-                    return res.status(500).json({ mensaje: 'Error al ejecutar el script de Python' });
-                }
+            // Ejecuta el script Python para generar el archivo TIFF
+            const comandoPython = `python3 /geomotica/procesos/generar_raster.py ${id} ${nombreAnalisis}`;
+            const options = { maxBuffer: 1024 * 1024 * 50 }; // 50 MB
+
+            try {
+                const { stdout, stderr } = await exec(comandoPython, options);
                 if (stderr) {
                     console.error(`Stderr: ${stderr}`);
                     return res.status(500).json({ mensaje: 'Error en el script de Python' });
                 }
+                console.log("Script de Python ejecutado exitosamente: ", stdout);
 
-                console.log("Script de Python ejecutado exitosamente.");
-
-                setTimeout(async () => {
-                    const existeArchivoGenerado = await archivo.exists();
-                    if (!existeArchivoGenerado[0]) {
-                        return res.status(404).json({ mensaje: 'Archivo no encontrado después de generación' });
-                    }
-
-                    console.log("Enviando archivo generado...");
-                    res.setHeader('Content-Type', 'image/tiff');
-                    return archivo.createReadStream().pipe(res);
-                }, 5000);
-            });
-        } else {
-            console.log("Enviando archivo existente...");
-            res.setHeader('Content-Type', 'image/tiff');
-            return archivo.createReadStream().pipe(res);
+                // Verifica de nuevo si el archivo existe después de la generación
+                const [existeArchivoGenerado] = await archivo.exists();
+                if (!existeArchivoGenerado) {
+                    return res.status(404).json({ mensaje: 'Archivo no encontrado después de generación' });
+                }
+            } catch (error) {
+                console.error(`Error al ejecutar el script de Python: ${error.message}`);
+                return res.status(500).json({ mensaje: 'Error al ejecutar el script de Python' });
+            }
         }
+
+        // Genera y envía la URL del archivo
+        const [url] = await archivo.getSignedUrl({
+            action: 'read',
+            expires: Date.now() + 3600 * 1000, // URL válida por 1 hora
+        });
+
+        console.log("ESTA ES LA URL GENERADA:", url);
+        console.log("Enviando URL del archivo...");
+        return res.json({ url });
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({ mensaje: 'Error al obtener el archivo' });
+        console.error(`Error al procesar la solicitud: ${error}`);
+        return res.status(500).json({ mensaje: 'Error interno del servidor' });
     }
 };
 module.exports = {
