afe1c2d5fc6d3a6ab9535abd7dd2c7fcf0243ce2
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2024-04-23 17:02:57 -0600

    logs params
    Add, añado logs de parametros.

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -372,6 +372,7 @@ const dosisTeoricaAps = async(req, res) =>{
 }
 const humedadDelCultivo = async(req, res)  =>{
     const idAnalisis = req.params.ID_ANALISIS;
+    console.log("PARAMETROS: ", req.params);
     console.log("EL ID DEL ANALISISSSS ************************************************ : )", idAnalisis);
     const obtenerHumedadDelCultivoAps = await DashboardModel.obtenerHumedadDelCultivo(idAnalisis);
     console.log("ESTE ES EL DATO DE HUMEDAD DEL CULTIVO ****************************");
